37f0006fe128c4faa06be6a59eaa0b71691bee79
muhamadabduh <muhamad.abduh.muh@gmail.com>
2018-09-13 11:13:23 +0700

    fixing oop

--- oop/hacktiv8.js
+++ oop/hacktiv8.js
@@ -7,32 +7,64 @@ class Hacktiv8 {
         this._age = obj.age
         this._address = obj.address
         this._gender = obj.gender
+        this._students = []
+        this._managers = []
+        this._instructor = []
+    }
+
+    get students(){
+        return this._students
+    }
+
+    set students(student){
+        this._students.push(student)
+    }
+
+    get managers(){
+        return this._managers
+    }
+
+    set managers(manager){
+        this._managers.push(manager)
+    }
+
+    addStudent(student){
+        let abduh = new Student;
+        this.phase = student.phase;
     }
 }
 
 class Manager extends Hacktiv8{
-    constructor(obj){
-        // super()
-        this._salary = obj.salary
+    constructor(salary){
+        super(id, name, age, address, gender)
+        this._salary = salary
     }
 }
 
-class Instructor extends Hacktiv8{
-    constructor(obj){
-        // super()
+class Instructor{
+    constructor(){
     }
 }
 
-class Student extends Hacktiv8{
+class Student extends Hacktiv8 {
     constructor(obj){
-        // super()
+        super({
+            id : obj.id, 
+            name: obj.name,
+            age: obj.age,
+            address: obj.address,
+            gender: obj.gender
+        })
         this._phase = obj.phase
     }
 }
 
 // var abduh = new Student({id: 1, name: 'Abduh', age: 25, address: 'Bandung', gender: 'male', phase:'anchorFox'})
-var abduh = new Hacktiv8({id: 1, name: 'Abduh', age: 25, address: 'Bandung', gender: 'male'})
+var abduh = new Student({id: 1, name: 'Abduh', age: 25, address: 'Bandung', gender: 'male', phase : 'anchorFox'})
 console.log(abduh)
-// var abduhStudents =  new Student(abduh)
+// var hacktiv = new Hacktiv8(abduh)
+// console.log(hacktiv)
+
+// var abduhStudents =  new Student({id: 1, name: 'Abduh', age: 25, address: 'Bandung', gender: 'male', phase: 'anchorFox'})
 // console.log(abduhStudent)
 // var abduhStudent = new Student()
