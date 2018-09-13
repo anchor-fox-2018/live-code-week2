39db4b9c101c69be0c252620f93a26e67f78b6b6
Lundy Orlando <simplyeazy@gmail.com>
2018-09-13 11:14:36 +0700

    Release 2 Show All Salary

--- oop/oop.js
+++ oop/oop.js
@@ -20,8 +20,27 @@ class Company {
             console.log(`You're not a member of Hacktiv8!`);
         }
     }
-    showSalaries(){
-        
+    showSalaries() {
+        let validRights = false;
+        for (let i = 0; i < this._loggedInUser.length; i++) {
+            for (let j = 0; j < this._people.length; j++) {
+                if (this._people[j].name === this._loggedInUser[i]) {
+                    if (this._people[j].accessRight === "Manager") {
+                        validRights = true;
+                    }
+                }
+            }
+        }
+        if (validRights) {
+            for (let i = 0; i < this._people.length; i++) {
+                if (this._people[i].salary !== null) {
+                    console.log(`Name: ${this._people[i].name}, Salary: ${this._people[i].salary}`);
+                }
+            }
+        }
+        else {
+            console.log(`You don't have permission to access this feature!`);
+        }
     }
 }
 
@@ -51,7 +70,7 @@ class Manager extends PeopleInHacktiv8 {
         console.log(`Saya adalah seorang instruktur!`);
     }
     registerToCompany() {
-        hacktiv8._people.push({name: this._name, accessRight: "Manager"});
+        hacktiv8._people.push({ id: hacktiv8._people.length, name: this._name, accessRight: "Manager", salary: this._salary });
     }
 }
 
@@ -65,7 +84,7 @@ class Instructor extends PeopleInHacktiv8 {
         console.log(`Saya sedang belajar programming disini!`);
     }
     registerToCompany() {
-        hacktiv8._people.push({name: this._name, accessRight: "Instructor"});
+        hacktiv8._people.push({ id: hacktiv8._people.length, name: this._name, accessRight: "Instructor", salary: this._salary });
     }
 }
 
@@ -79,7 +98,7 @@ class Student extends PeopleInHacktiv8 {
         console.log(`Saya adalah seorang manager!`);
     }
     registerToCompany() {
-        hacktiv8._people.push({name: this._name, accessRight: "Student"});
+        hacktiv8._people.push({ id: hacktiv8._people.length, name: this._name, accessRight: "Student", salary: null, phase: this._phase });
     }
 }
 
@@ -88,7 +107,11 @@ const manager = new Manager(1, "John Kosasih", 15, "Mars", "Male", 100);
 const instructor1 = new Instructor(2, "Marry Kosasih", 16, "Pluto", "Female", 200);
 const instructor2 = new Instructor(3, "Kirry Kosasih", 17, "Saturnus", "Male", 300);
 const student = new Student(4, "Kurry Kosasih", 27, "Pluto", "Male", 1);
-
+//daftar ke kumpeni
 manager.registerToCompany();
-hacktiv8.login("Marry Kosasih");
+instructor1.registerToCompany();
+instructor2.registerToCompany();
+student.registerToCompany();
+//hacktiv8.login("Marry Kosasih");
 hacktiv8.login("John Kosasih");
+hacktiv8.showSalaries();
