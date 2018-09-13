da30aed6ec94bdfb13d41c0cc8c3bdab454e5d2d
awtian <instructor@MacBook-Air-2.local>
2018-09-13 09:09:23 +0700

    initial commit

---
+++ logic/index.js
@@ -0,0 +1,94 @@
+/*
+    PERKALIAN MATRIKS
+    ================
+*/
+
+function matriksMultiply(matriks1,matriks2){
+    // your code here 
+}
+
+/* 
+    testcase 1
+    ========== 
+*/
+
+let matriks1 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+let matriks2 = [
+    [6,5],
+    [3,4],
+    [2,1]
+]
+
+console.log(matriks1,matriks2)
+
+/*
+   [
+    [(1*6)+(2*3)+(3*2), (1*5)+(2*4)+(3*1)], 
+    [(4*6)+(5*3)+(6*2), (4*5)+(5*4)+(6*1)],
+    [(7*6)+(8*3)+(9*2), (7*5)+(8*4)+(9*1)]
+   ]
+
+   [
+    [18,16],
+    [51,46],
+    [84,76]
+   ]
+*/
+
+
+/* 
+    testcase 2
+    ========== 
+*/
+
+let matriks3 = [
+    [1,2,3,4],
+    [5,6,7,8],
+   
+]
+
+let matriks4 = [
+    [1,2],
+    [3,4],
+    [5,6],
+    [7,8]
+]
+
+console.log(matriksMultiply(matriks3,matriks4))
+/*
+    [ 
+      [ 50, 60 ], 
+      [ 114, 140 ] 
+    ]
+*/
+
+/* 
+    testcase 3
+    ========== 
+*/
+
+let matriks5 = [
+    [1,2],
+    [3,4],
+   
+]
+
+let matriks6 = [
+    [1,2],
+    [3,4],
+    
+]
+
+console.log(matriksMultiply(matriks5,matriks6))
+
+/* 
+   [ 
+    [ 7, 10 ], 
+    [ 15, 22 ]  
+   ]
+*/
