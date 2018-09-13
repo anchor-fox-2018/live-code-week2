2b8624aa0abc846dd7cba0d8a1f83238e1190fc7
Lundy Orlando <simplyeazy@gmail.com>
2018-09-13 09:21:29 +0700

    Release 0 OOP

---
+++ oop/oop.js
@@ -0,0 +1,43 @@
+
+class PeopleInHacktiv8 {
+
+}
+class Manager {
+    constructor() {
+        this._id = id;
+        this._name = name;
+        this._age = age;
+        this._address = address;
+        this._gender = gender;
+        this._salary = salary;
+    }
+    greet() {
+        return `Halo! Saya salah satu member Hacktiv8!`;
+    }
+}
+class Instructor {
+    constructor() {
+        this._id = id;
+        this._name = name;
+        this._age = age;
+        this._address = address;
+        this._gender = gender;
+        this._salary = salary;
+    }
+    greet() {
+        return `Halo! Saya salah satu member Hacktiv8!`;
+    }
+}
+class Student {
+    constructor() {
+        this._id = id;
+        this._name = name;
+        this._age = age;
+        this._address = address;
+        this._gender = gender;
+        this._phase = phase;
+    }
+    greet() {
+        return `Halo! Saya salah satu member Hacktiv8!`;
+    }
+}
