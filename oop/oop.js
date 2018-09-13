5bef9529e0384eee50967e1e220de3682c7f1261
Lundy Orlando <simplyeazy@gmail.com>
2018-09-13 09:35:19 +0700

    Release 1 OOP

--- oop/oop.js
+++ oop/oop.js
@@ -1,29 +1,52 @@
 class PeopleInHacktiv8 {
-    constructor() {
-        this._id = id;
-        this._name = name;
-        this._age = age;
-        this._address = address;
-        this._gender = gender;
+    constructor(id, name, age, address, gender) {
+        this._id = id || 0;
+        this._name = name || "noname";
+        this._age = age || 0;
+        this._address = address || "";
+        this._gender = gender || "";
+    }
+    greet() {
+        console.log(`Halo! Saya salah satu member Hacktiv8!`);
     }
 }
 class Manager extends PeopleInHacktiv8 {
-    constructor() {
+    constructor(salary) {
         super();
         this._salary = salary;
     }
-
+    greet() {
+        super.greet();
+        console.log(`Saya adalah seorang instruktur!`);
+    }
 }
 class Instructor extends PeopleInHacktiv8 {
-    constructor() {
+    constructor(salary) {
         super();
         this._salary = salary;
+    }
+    greet() {
+        super.greet();
+        console.log(`Saya sedang belajar programming disini!`);
     }
 }
 class Student extends PeopleInHacktiv8 {
-    constructor() {
+    constructor(phase) {
         super();
         this._phase = phase;
     }
+    greet() {
+        super.greet();
+        console.log(`Saya adalah seorang manager!`);
+    }
 
 }
+
+let instruk = new Instructor();
+instruk.greet();
+
+let studen = new Student();
+studen.greet();
+
+let mana = new Manager();
+mana.greet();
