f5e82d4e58b357d13cc93fab3e7417387ac86db4
Lundy Orlando <simplyeazy@gmail.com>
2018-09-13 09:58:29 +0700

    Release 2 OOP

--- oop/oop.js
+++ oop/oop.js
@@ -1,3 +1,14 @@
+class Company {
+    constructor(companyName, limit, people, loggedInUser) {
+        this._companyName = companyName || "Hacktiv8";
+        this._limit = limit || 10;
+        this._loggedInUser = loggedInUser || null;
+        this._people = [];
+    }
+}
+
+
+
 class PeopleInHacktiv8 {
     constructor(id, name, age, address, gender) {
         this._id = id || 0;
@@ -11,8 +22,8 @@ class PeopleInHacktiv8 {
     }
 }
 class Manager extends PeopleInHacktiv8 {
-    constructor(salary) {
-        super();
+    constructor(id, name, age, address, gender, salary) {
+        super(id, name, age, address, gender);
         this._salary = salary;
     }
     greet() {
@@ -21,8 +32,8 @@ class Manager extends PeopleInHacktiv8 {
     }
 }
 class Instructor extends PeopleInHacktiv8 {
-    constructor(salary) {
-        super();
+    constructor(id, name, age, address, gender, salary) {
+        super(id, name, age, address, gender);
         this._salary = salary;
     }
     greet() {
@@ -31,8 +42,8 @@ class Instructor extends PeopleInHacktiv8 {
     }
 }
 class Student extends PeopleInHacktiv8 {
-    constructor(phase) {
-        super();
+    constructor(id, name, age, address, gender, phase) {
+        super(id, name, age, address, gender);
         this._phase = phase;
     }
     greet() {
@@ -41,12 +52,13 @@ class Student extends PeopleInHacktiv8 {
     }
 
 }
+let manager = new Manager(1, "John Kosasih", 15, "Mars", "Male", 100);
+console.log(`${manager._id} ${manager._name} ${manager._age} ${manager._address} ${manager._gender}`)
+let instructor1 = new Instructor(2, "Marry Kosasih", 16, "Pluto", "Female", 200);
+console.log(`${instructor1._id} ${instructor1._name} ${instructor1._age} ${instructor1._address} ${instructor1._gender}`)
 
-let instruk = new Instructor();
-instruk.greet();
+let instructor2 = new Instructor(3, "Kirry Kosasih", 17, "Saturnus", "Male", 300);
+console.log(`${instructor2._id} ${instructor2._name} ${instructor2._age} ${instructor2._address} ${instructor2._gender}`)
 
-let studen = new Student();
-studen.greet();
-
-let mana = new Manager();
-mana.greet();
+let student = new Student(4, "Kurry Kosasih", 27, "Pluto", "Male", 1);
+console.log(`${student._id} ${student._name} ${student._age} ${student._address} ${student._gender}`)
